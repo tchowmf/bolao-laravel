065991afbfe510cfb608bb469b4237fb0840a5cb
Vitor Ferreira <vitor.marostica@gmail.com>
2024-03-08 11:45:47 -0300

    fix: href on sidebar brand

--- resources/views/Template/blank.blade.php
+++ resources/views/Template/blank.blade.php
@@ -31,7 +31,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../">
                 <div class="sidebar-brand-icon">
                     <img src="/img/F1.svg" style="width: 6rem;"/>
                 </div>
