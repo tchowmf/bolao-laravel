5a00badc0643d09a52502c75f73f0204adc82f65
Vitor Ferreira <vitor.marostica@gmail.com>
2024-03-21 15:03:46 -0300

    update: custom error 404 page add

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('Template.blank')
+@section('title', "Resultados do Bolão - GP Bahrein 2024")
+
+@section('contents')
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- 404 Error Text -->
+    <div class="text-center">
+        <div class="error mx-auto" data-text="404">404</div>
+        <p class="lead text-gray-800 mb-5">Página não encontrada!</p>
+        <p class="text-gray-500 mb-0"><img src="/img/vermelha.png"/> Parece que você encontrou um erro. Bandeira vermelha <img src="/img/vermelha.png"/></p>
+        <a href="javascript:history.go(-1)">Voltar</a>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+@endsection
